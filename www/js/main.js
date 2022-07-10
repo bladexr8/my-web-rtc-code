76fe45cb0d5f31b83d8879fdf2b49194c2e19356
Stephen Moss <bladexr8@gmail.com>
2022-07-10 10:38:04 +1000

    call UI and basic js functionality

---
+++ www/js/main.js
@@ -0,0 +1,116 @@
+/***
+ * Excerpted from "Programming WebRTC",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit https://pragprog.com/titles/ksrtc for more book information.
+***/
+'use strict';
+
+/**
+ *  Global Variables: $self and $peer
+ */
+
+
+
+/**
+ *  Signaling-Channel Setup
+ */
+
+const namespace = prepareNamespace(window.location.hash, true);
+
+
+
+
+/**
+ * =========================================================================
+ *  Begin Application-Specific Code
+ * =========================================================================
+ */
+
+
+/**
+ *  User-Interface Setup
+ */
+
+document.querySelector('#header h1')
+  .innerText = 'Welcome to Room #' + namespace;
+
+document.querySelector('#call-button')
+  .addEventListener('click', handleCallButton);
+
+
+
+/**
+ *  User-Media Setup
+ */
+
+
+
+
+
+/**
+ *  User-Interface Functions and Callbacks
+ */
+function handleCallButton(event) {
+    console.log('Call button clicked! Named callback function active!')
+}
+
+
+/**
+ *  User-Media Functions
+ */
+
+
+
+/**
+ *  Call Features & Reset Functions
+ */
+
+
+
+
+/**
+ *  WebRTC Functions and Callbacks
+ */
+
+
+
+
+
+/**
+ * =========================================================================
+ *  End Application-Specific Code
+ * =========================================================================
+ */
+
+
+
+/**
+ *  Reusable WebRTC Functions and Callbacks
+ */
+
+
+
+/**
+ *  Signaling-Channel Functions and Callbacks
+ */
+
+
+
+
+/**
+ *  Utility Functions
+ */
+function prepareNamespace(hash, set_location) {
+  let ns = hash.replace(/^#/, ''); // remove # from the hash
+  if (/^[0-9]{7}$/.test(ns)) {
+    console.log('Checked existing namespace', ns);
+    return ns;
+  }
+  ns = Math.random().toString().substring(2, 9);
+  console.log('Created new namespace', ns);
+  if (set_location) window.location.hash = ns;
+  return ns;
+}
